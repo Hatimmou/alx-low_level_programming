54e6a9265efab10b93b67c03a25eea05f79bb6f4
Hatimmou <mounaji97@gmail.com>
2023-06-19 19:45:52 -0700

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.githubusercontent.com/Hatimmou/alx-low_level_programming/blob/main/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp/ https://raw.githubusercontent.com/Hatimmou/alx-low_level_programming/main/0x18-dynamic_libraries/101-make_me_win.sh
 export LD_PRELOAD=/tmp/nrandom.so
